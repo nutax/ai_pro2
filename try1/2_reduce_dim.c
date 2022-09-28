52d2ccf670ad9a0054e03fb0bb6db89f2fa31317
Jose Huby <jose.huby@utec.edu.pe>
2022-09-28 13:20:27 -0500

    reduce_dim_stats

--- try1/2_reduce_dim.c
+++ try1/2_reduce_dim.c
@@ -6,12 +6,14 @@
 int main();
 void read_raw(const char *path, struct sdf *sdf);
 void test_raw(struct sdf *sdf);
+void stats(struct df *df);
 
 struct sdf _sdf;
 
 int main(){
     read_raw("sdf.o", &_sdf);
     test_raw(&_sdf);
+    stats(&(_sdf.train));
     return 0;
 }
 
@@ -24,4 +26,34 @@ void read_raw(const char *path, struct sdf *sdf){
 }
 void test_raw(struct sdf *sdf){
     printf("Train: %d    Test: %d", sdf->train.size, sdf->test.size);
+}
+
+
+void fr(int *data, int buckets, int size);
+int mean(int *data, int size);
+int sd(int *data, int mean, int size);
+int median(int *data, int size);
+
+void stats(struct df *df){
+    int i;
+    int label_fr[26] = {0};
+    int pixel_sd[PIXELS], pixel_mean, pixel_sd_mean, pixel_sd_sd, pixel_sd_median;
+    
+    fr(df->label, label_fr, df->size);
+
+    for(i = 0; i<PIXELS; i++){
+        pixel_mean = mean(df->pixel[i], df->size);
+        pixel_sd[i] = sd(df->pixel[i], pixel_mean, df->size);
+    }
+
+    pixel_sd_mean = mean(pixel_sd, df->size);
+    pixel_sd_sd = sd(pixel_sd, pixel_sd_mean, df->size);
+    pixel_sd_median = meadian(pixel_sd, df->size);
+
+    for(int i = 0; i<26; i++) printf("[%d]%d  ", i, label_fr[i]);
+    printf("\n\n");
+    printf("SD Mean: %d\n", pixel_sd_mean);
+    printf("SD SD: %d\n", pixel_sd_sd);
+    printf("SD Median: %d\n", pixel_sd_median);
+
 }
